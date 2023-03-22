9cbb01cfa2980d6c79dbc1065977d98adb493ac7
chococuki <kkang13277@naver.com>
2023-03-22 23:36:05 +0900

    [Bronze V] Title: A+B - 3, Time: 124 ms, Memory: 13096 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+			sb.append((sc.nextInt()+sc.nextInt())+"\n");
+		}
+        
+        System.out.println(sb.toString());
+    }
+}
