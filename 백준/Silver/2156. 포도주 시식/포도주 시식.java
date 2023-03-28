ae429d1c1ebec51ec1bdc28e89db4df6bf32cd79
chococuki <kkang13277@naver.com>
2023-03-28 11:50:32 +0900

    [Silver I] Title: 포도주 시식, Time: 260 ms, Memory: 24144 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -16,24 +16,8 @@ public class Main {
 		res[1] = arr[1];
 		if(n>=2) res[2] = arr[1] + arr[2];
 		
-		boolean use = true;	//인덱스가 i일때 두번 연속 선택 하였는지
 		for (int i = 3; i <= n; i++) {
-			if(!use) {
-				if((res[i-2]+arr[i]) > (res[i-1]+arr[i])) {
-					res[i] = res[i-2]+arr[i];
-				} else {
-					res[i] = res[i-1]+arr[i];
-					use = true;
-				}
-			} else {
-				res[i] = Math.max(res[i-1], res[i-2]+arr[i]);
-				use = false;
-				
-				if((res[i-3]+arr[i-1]+arr[i]) > res[i]) {
-					res[i] = res[i-3]+arr[i-1]+arr[i];
-					use = true;
-				}
-			}
+			res[i] = Math.max((res[i-3]+arr[i-1]+arr[i]), Math.max(res[i-1], res[i-2]+arr[i]));
 		}
 		
 		System.out.println(res[n]);
