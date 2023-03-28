e11c5e329784b06c4f238a7ad50ebc2fdde5a37e
chococuki <kkang13277@naver.com>
2023-03-28 13:43:49 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 12096 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,40 +1,39 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-
+public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int minimum = Integer.MAX_VALUE;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int[][]house = new int [N][N];
-		int [][] DP = new int [N+1][3];
+		int T = Integer.parseInt(st.nextToken());
 		
-		for(int i = 0; i < N; i++) {
-			String [] input = br.readLine().split(" ");
-			for(int j = 0; j < 3; j++) {
-				house[i][j] = Integer.parseInt(input[j]);
-			}
+		int[][] cost = new int[T+1][3];
+		for(int i=1; i<=T; i++) {
+			st = new StringTokenizer(br.readLine());
+			cost[i][0] = Integer.parseInt(st.nextToken());
+			cost[i][1] = Integer.parseInt(st.nextToken());
+			cost[i][2] = Integer.parseInt(st.nextToken());
 		}
 		
-
+		int[][] road = new int[T+1][3];
 		
-		for(int i = 1; i < N+1; i++) {
-			DP[i][0] = Math.min(DP[i-1][1]+house[i-1][0],DP[i-1][2]+ house[i-1][0]);
-			DP[i][1] = Math.min(DP[i-1][0]+house[i-1][1],DP[i-1][2]+ house[i-1][1]);
-			DP[i][2] = Math.min(DP[i-1][0]+house[i-1][2],DP[i-1][1]+ house[i-1][2]);
-	
+		//road[i][j] -> i번째 집에 칠해진 색=j
+		road[1][0] = cost[1][0];
+		road[1][1] = cost[1][1];
+		road[1][2] = cost[1][2];
+		
+		for(int i=2; i<=T; i++) {
+			// i번째에 j 색을 칠하기 위한 최소 비용을 가져옴
+			road[i][0] = Math.min(road[i-1][1], road[i-1][2])+cost[i][0];
+			road[i][1] = Math.min(road[i-1][0], road[i-1][2])+cost[i][1];
+			road[i][2] = Math.min(road[i-1][0], road[i-1][1])+cost[i][2];
 		}
-
-		for (int i = 0; i < 3; i++) {
-			minimum = Math.min(DP[N][i],minimum);
-		}
 		
-		System.out.println(minimum);
+		int result = Math.min(Math.min(road[T][0], road[T][1]), road[T][2]);
 		
+		System.out.println(result);
 	}
-
 }
