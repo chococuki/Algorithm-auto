95778ea3e45d38a273ccbe0019b3481ee625f03a
chococuki <kkang13277@naver.com>
2023-03-30 17:12:59 +0900

    [Gold IV] Title: 스도쿠, Time: 1152 ms, Memory: 298780 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -2,106 +2,83 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-
-class node {
-	int x;
-	int y;
-
-	public node(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
 
 public class Main {
+	static int[][] sudoku;
+	static StringBuilder sb = new StringBuilder();
 
-	static String[] in;
-	static int cnt = 0;
-	static int[][] map = new int[9][9];
-	static ArrayList<node> nodeAry = new ArrayList<>();
+	private static boolean makeSudoku(int start) {
+		boolean hasZero = false;
+		ArrayList<Integer> numbers = new ArrayList<Integer>(); // 가능한 수 리스트
+		for (int k = start / 9; k < 81; k++) {
+			int i = k / 9;
+			int j = k % 9;
+			if (sudoku[i][j] == 0) {
+				hasZero = true;
+				getNumbers(i, j, numbers); // 가능한 수 찾기
+				for (int n : numbers) {
+					sudoku[i][j] = n;
+					if (makeSudoku(k + 1)) {
+						return true;
+					}
+				}
+				sudoku[i][j] = 0;
+				return false;
+			}
+		}
+		
+		//더이상 빈칸이 없으면 종료
+		if (!hasZero) {
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					sb.append(sudoku[i][j]);
+				}
+				sb.append("\n");
+			}
+			return true;
+		}
+		return false;
+	}
+
+	private static void getNumbers(int i, int j, ArrayList<Integer> numbers) {
+		numbers.clear();
+		boolean[] used = new boolean[10];
+		// 가로, 세로에 있는 수 제외
+		for (int a = 0; a < 9; a++) {
+			used[sudoku[i][a]] = true;
+			used[sudoku[a][j]] = true;
+		}
+		// 네모칸에 있는 수 제외
+		int row = (i / 3) * 3;
+		int col = (j / 3) * 3;
+		for (int a = row; a < row + 3; a++) {
+			for (int b = col; b < col + 3; b++) {
+				used[sudoku[a][b]] = true;
+			}
+		}
+		// 가능한 수 리스트 생성
+		for (int n = 1; n <= 9; n++) {
+			if (!used[n]) {
+				numbers.add(n);
+			}
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str;
+
+		sudoku = new int[9][9];
 
 		for (int i = 0; i < 9; i++) {
-			in = br.readLine().split("");
+			str = br.readLine().split("");
 			for (int j = 0; j < 9; j++) {
-				int now = Integer.parseInt(in[j]);
-				map[i][j] = now;
-				if (now == 0) {
-					node nd = new node(i, j);
-					cnt++;
-					nodeAry.add(nd);
-				}
+				sudoku[i][j] = Integer.parseInt(str[j]);
 			}
 		}
 
-		dfs(0);
+		makeSudoku(0);
+
+		System.out.println(sb.toString());
 	}
-
-	public static void dfs(int depth) {
-
-		if (depth == cnt) {
-			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++) {
-					System.out.print(map[i][j]);
-				}
-				System.out.println();
-			}
-			System.exit(0);
-		}
-
-		for (int i = 1; i < 10; i++) {
-
-			int nowX = nodeAry.get(depth).x;
-			int nowY = nodeAry.get(depth).y;
-			
-			map[nowX][nowY] = i;
-			if (check(nowX, nowY))dfs(depth + 1);
-			map[nowX][nowY] = 0;
-		}
-
-	}
-
-	public static boolean check(int x, int y) {
-
-		int value = map[x][y];
-
-
-		// 열 조사
-		for (int i = 0; i < 9; i++) {
-			if (i != x) {
-				if (map[i][y] == value) {
-					return false;
-				}
-			}
-		}
-
-		// 행 조사
-		for (int i = 0; i < 9; i++) {
-			if (i != y) {
-				if (map[x][i] == value) {
-					return false;
-				}
-			}
-		}
-
-		// 사각형 검사
-		int startX = (x / 3) * 3;
-		int startY = (y / 3) * 3;
-
-		for (int i = startX; i < startX + 3; i++) {
-			for (int j = startY; j < startY + 3; j++) {
-				if (i != x && j != y) {
-					if (map[i][j] == value) {
-						return false;
-					}
-				}
-			}
-		}
-
-		return true;
-	}
-
 }
