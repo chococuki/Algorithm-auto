07af077e16b43608abd48e2a14d8897f84ab4da5
chococuki <kkang13277@naver.com>
2023-03-30 12:02:26 +0900

    [Gold V] Title: 평범한 배낭, Time: 152 ms, Memory: 51300 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,43 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input [] = br.readLine().split(" ");
-		int N = Integer.parseInt(input[0]);
-		int K = Integer.parseInt(input[1]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int [] W = new int[N+1];
-		int [] V = new int[N+1];
-		for(int i = 1; i < N+1; i++) {
-			String [] in = br.readLine().split(" ");
-			int w = Integer.parseInt(in[0]);
-			int v = Integer.parseInt(in[1]);
-			
-			W[i] = w;
-			V[i] = v;
-			
-			
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int weight[] = new int[N+1];
+		int value[] = new int[N+1];
+		
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			weight[i] = Integer.parseInt(st.nextToken());
+			value[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int [][] DP = new int[N+1][K+1];
+		int board[][] = new int[N+1][K+1];
 		
-		for(int i = 1; i < N+1; i++) {
-			for(int j = 1; j < K+1; j++) {
-				if(j < W[i])DP[i][j] = DP[i-1][j];
-				
-				else if(j >= W[i])DP[i][j] = Math.max(DP[i-1][j], DP[i-1][j-W[i]]+V[i]);
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= K; j++) {
+				if(weight[i] > j) {
+					board[i][j] = board[i-1][j];
+				} else {
+					board[i][j] = Math.max(board[i-1][j], board[i-1][j-weight[i]]+value[i]);
+				}
 			}
-			
 		}
 		
-		System.out.println(DP[N][K]);
-		
-		
+		System.out.println(board[N][K]);
 	}
-
 }
