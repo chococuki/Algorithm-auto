f98826b8d162518194655665200ae2bfb69df25e
chococuki <kkang13277@naver.com>
2023-04-06 14:19:25 +0900

    [Gold II] Title: 퍼즐, Time: 928 ms, Memory: 122428 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -6,108 +6,123 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static final String result = "123456780";
-	static HashSet<String> hash = new HashSet<>();
-	static PriorityQueue<Map> fastRoute = new PriorityQueue<>();
-	
-	static class Map implements Comparable<Map>{
-		int[][] arr;
-		int dept;
-		
-		public Map(int[][] arr, int dept) {
+	// 정답 상수 문자열
+	static final String RESULT_STRING = "123456780";
+
+	// 방문한 배열을 저장하기 위한 HashSet
+	static HashSet<String> visited = new HashSet<>();
+
+	// 빈 공간을 움직이는 최적 경로를 저장하기 위한 PriorityQueue
+	static PriorityQueue<MoveInfo> movePriorityQueue = new PriorityQueue<>();
+
+	static class MoveInfo implements Comparable<MoveInfo> {
+		// 2차원 배열
+		int[][] puzzle;
+		// 움직인 횟수
+		int moveCount;
+
+		public MoveInfo(int[][] puzzle, int moveCount) {
 			super();
-			this.arr = arr;
-			this.dept = dept;
+			this.puzzle = puzzle;
+			this.moveCount = moveCount;
 		}
 
 		@Override
-		public int compareTo(Map o) {
-			return Integer.compare(this.dept, o.dept);
+		public int compareTo(MoveInfo o) {
+			return Integer.compare(this.moveCount, o.moveCount);
 		}
 	}
-	
-	private static int startMove() {
-		int[] dr = {1, -1, 0, 0};
-		int[] dc = {0, 0, 1, -1};
-		
-		while (!fastRoute.isEmpty()) {
-			Map now = fastRoute.poll();
-			
-			int index0 = find0(now.arr);
-			
+
+	// 시작 위치에서 최적 경로를 찾기 위한 메소드
+	private static int findOptimalRoute() {
+		// 상하좌우 이동을 위한 dr, dc 배열
+		int[] dr = { 1, -1, 0, 0 };
+		int[] dc = { 0, 0, 1, -1 };
+
+		while (!movePriorityQueue.isEmpty()) {
+			MoveInfo now = movePriorityQueue.poll();
+			int indexOfZero = findIndexOfZero(now.puzzle);
+
 			for (int i = 0; i < 4; i++) {
-				int row = index0/3 + dr[i];
-				int col = index0%3 + dc[i];
-				
-				if(row>=0 && row<3 && col>=0 && col<3) {
-					int tmp = now.arr[index0/3][index0%3];
-					now.arr[index0/3][index0%3] = now.arr[row][col];
-					now.arr[row][col] = tmp;
-					String strArray = arrayToString(now.arr);
-					if(strArray.equals(result)) {
-						return now.dept+1;
-					} else if(!hash.contains(strArray)) {
-						hash.add(strArray);
-						fastRoute.add(new Map(copyArray(now.arr), now.dept+1));
+				int row = indexOfZero / 3 + dr[i];
+				int col = indexOfZero % 3 + dc[i];
+
+				// 퍼즐의 경계를 넘어가지 않게 하기 위한 조건문
+				if (row >= 0 && row < 3 && col >= 0 && col < 3) {
+					int tmp = now.puzzle[indexOfZero / 3][indexOfZero % 3];
+					now.puzzle[indexOfZero / 3][indexOfZero % 3] = now.puzzle[row][col];
+					now.puzzle[row][col] = tmp;
+					String strArray = arrayToString(now.puzzle);
+					if (strArray.equals(RESULT_STRING)) {
+						return now.moveCount + 1;
+					} else if (!visited.contains(strArray)) {
+						visited.add(strArray);
+						movePriorityQueue.add(new MoveInfo(copyArray(now.puzzle), now.moveCount + 1));
 					}
-					now.arr[row][col] = now.arr[index0/3][index0%3];
-					now.arr[index0/3][index0%3] = tmp;
+					now.puzzle[row][col] = now.puzzle[indexOfZero / 3][indexOfZero % 3];
+					now.puzzle[indexOfZero / 3][indexOfZero % 3] = tmp;
 				}
 			}
 		}
-		
+
 		return -1;
 	}
-	
-	private static int find0(int[][] arr) {
+
+	// 2차원 배열에서 0의 인덱스를 찾는 메소드
+	private static int findIndexOfZero(int[][] puzzle) {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				if(arr[i][j] == 0) return i*3+j;
+				if (puzzle[i][j] == 0)
+					return i * 3 + j;
 			}
 		}
 		return -1;
 	}
-	
-	private static int[][] copyArray(int[][] arr) {
-		int[][] tmp = new int[3][3];
-		
+
+	// 2차원 배열을 복사하여 반환하는 메소드
+	private static int[][] copyArray(int[][] puzzle) {
+		int[][] copiedArray = new int[3][3];
+
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				tmp[i][j] = arr[i][j];
+				copiedArray[i][j] = puzzle[i][j];
 			}
 		}
-		
-		return tmp;
+
+		return copiedArray;
 	}
-	
-	private static String arrayToString(int[][] arr) {
+
+	// 2차원 배열을 문자열로 변환하여 반환하는 메소드
+	private static String arrayToString(int[][] puzzle) {
 		StringBuilder sb = new StringBuilder();
-		
+
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				sb.append(arr[i][j]);
+				sb.append(puzzle[i][j]);
 			}
 		}
-		
+
 		return sb.toString();
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		int[][] arr = new int[3][3];
-		
+
+		int[][] puzzle = new int[3][3];
+
+		// 퍼즐 초기 상태 입력
 		for (int i = 0; i < 3; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 3; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				puzzle[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		fastRoute.add(new Map(arr, 0));
-		
-		System.out.println(!arrayToString(arr).equals(result) ? startMove() : 0);
-		
+
+		// 초기 상태를 우선순위 큐에 추가
+		movePriorityQueue.add(new MoveInfo(puzzle, 0));
+
+		// 초기 상태가 정답이 아닌 경우 최적 경로를 찾아 출력
+		System.out.println(!arrayToString(puzzle).equals(RESULT_STRING) ? findOptimalRoute() : 0);
 	}
 }
