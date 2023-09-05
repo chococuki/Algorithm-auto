e71b173f4f52c5fc7bd221c68eb10d11e7fe2ef0
hyunduk <kkang13277@naver.com>
2023-09-05 20:37:30 +0900

    [unrated] Title: 점 찍기, Time: 0.14 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
@@ -4,7 +4,7 @@ class Solution {
         long answer = 0;
         
         for(int i = 0; i <= d; i+=k) {
-            int tmp = (int)(Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2)));
+            long tmp = (long)(Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2)));
             answer += (tmp / k) + 1;    // 0 포함을 위한 +1
         }
         
