5e3451176e03703cd9fffb67aa68af5a940bd08a
hyunduk <kkang13277@naver.com>
2023-09-06 20:37:30 +0900

    [unrated] Title: 롤케이크 자르기, Time: 26.76 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,27 +3,36 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         
-        // map에 토핑종류, 갯수 저장
-        Map<Integer, Integer> mapA = new LinkedHashMap<>();
-        Map<Integer, Integer> mapB = new LinkedHashMap<>();
-        
-        // 한쪽에 모든 토핑
+        int maxNum = 0;
         for(int i : topping) {
-            mapA.put(i, mapA.getOrDefault(i, 0) + 1);
+            maxNum = Math.max(maxNum, i);
         }
         
+        int[] array = new int[maxNum + 1];
+        int countLeft = 0;
+        for(int i : topping) {
+            if(array[i] == 0) {
+                countLeft++;
+            }
+            array[i]++;
+        }
+
+        
         // 순서대로 이동하며 토핑 갯수 비교
+        boolean[] hasTopping = new boolean[maxNum + 1];
+        int countRight = 0;
         int answer = 0;
         for(int i : topping) {
-            mapB.put(i, mapA.getOrDefault(i, 0) + 1);
-            
-            if(mapA.get(i) == 1) {
-                mapA.remove(i);
-            } else {
-                mapA.put(i, mapA.get(i) - 1);
+            if(!hasTopping[i]) {
+                hasTopping[i] = true;
+                countRight++;
             }
-            
-            if(mapA.size() == mapB.size()) {
+            array[i]--;
+            if(array[i] == 0) {
+                countLeft--;
+            }
+
+            if(countRight == countLeft) {
                 answer++;
             }
         }
