0af3afb9a98af1a26b8c73d9da13fec77332d91e
hyunduk <kkang13277@naver.com>
2023-09-11 20:25:03 +0900

    [Gold III] Title: 미로 탈출하기, Time: 512 ms, Memory: 65260 KB -BaekjoonHub

--- 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.java
+++ 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 
 public class Main {
     static int N, M;
+    static String[][] map;
+    static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,17 +15,16 @@ public class Main {
         N = Integer.parseInt(str[0]);
         M = Integer.parseInt(str[1]);
 
-        String[][] map = new String[N][M];
+        map = new String[N][M];
+        visited = new boolean[N][M];
         for(int n = 0; n < N; n++) {
             map[n] = br.readLine().split("");
         }
 
         for(int n = 0; n < N; n++) {
             for(int m = 0; m < M; m++) {
-                if(map[n][m].equals("U") || map[n][m].equals("R") ||
-                        map[n][m].equals("D") || map[n][m].equals("L")) {
-                    map[n][m] = isNewRoute(n, m, map);
-
+                if(!visited[n][m]) {
+                    map[n][m] = isNewRoute(n, m);
                 }
             }
         }
@@ -39,34 +40,39 @@ public class Main {
         System.out.println(answer);
     }
 
-    public static String isNewRoute(int n, int m, String[][] map) {
+    public static String isNewRoute(int n, int m) {
         int newN = -1, newM = -1;
 
-        if(map[n][m].equals("U")) {
-            newN = n - 1;
-            newM = m;
-        } else if(map[n][m].equals("R")) {
-            newN = n;
-            newM = m + 1;
-        } else if(map[n][m].equals("D")) {
-            newN = n + 1;
-            newM = m;
-        } else if(map[n][m].equals("L")) {
-            newN = n;
-            newM = m - 1;
-        } else if(map[n][m].equals("0")) {
-            return "0";
-        } else if(map[n][m].equals("1")) {
-            return "1";
-        } else if(map[n][m].equals("-1")) {
-            return "0";
+        if(!visited[n][m]) {
+            visited[n][m] = true;
+
+            if (map[n][m].equals("U")) {
+                newN = n - 1;
+                newM = m;
+            } else if (map[n][m].equals("R")) {
+                newN = n;
+                newM = m + 1;
+            } else if (map[n][m].equals("D")) {
+                newN = n + 1;
+                newM = m;
+            } else if (map[n][m].equals("L")) {
+                newN = n;
+                newM = m - 1;
+            }
+        } else {
+            if(map[n][m].equals("1")) {
+                return "1";
+            } else {
+                map[n][m] = "0";
+                return "0";
+            }
         }
 
         if(newN >= 0 && newN < N && newM >= 0 && newM < M) {
-            map[n][m] = "-1";
-            map[n][m] = isNewRoute(newN, newM, map);
+            map[n][m] = isNewRoute(newN, newM);
             return map[n][m];
         } else {
+            map[n][m] = "1";
             return "1";
         }
     }
