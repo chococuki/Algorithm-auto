5db227418dd50e5c3274127c766cdbdc9e02f4a2
Hyunduk <kkang13277@naver.com>
2023-11-02 22:52:23 +0900

    [Gold V] Title: 수 고르기, Time: 384 ms, Memory: 27652 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -12,20 +12,20 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
-		Long[] numbers = new Long[N];
+		int[] numbers = new int[N];
 
 		for (int i = 0; i < N; i++) {
-			numbers[i] = Long.parseLong(br.readLine());
+			numbers[i] = Integer.parseInt(br.readLine());
 		}
 
 		Arrays.sort(numbers);
 
 		int front = 0;
 		int back = 0;
-		long min = Long.MAX_VALUE;
+		int min = Integer.MAX_VALUE;
 
 		while (front < N && front >= back) {
-			long between = numbers[front] - numbers[back];
+			int between = Math.abs(numbers[front] - numbers[back]);
 			if (between > M) {
 				min = Math.min(min, between);
 				back++;
