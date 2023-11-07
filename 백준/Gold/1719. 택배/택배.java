6de8e66e70b49e3f594568c6052eec380f07c747
Hyunduk <kkang13277@naver.com>
2023-11-07 23:06:24 +0900

    [Gold III] Title: 택배, Time: 1664 ms, Memory: 172368 KB -BaekjoonHub

---
+++ 백준/Gold/1719. 택배/택배.java
@@ -0,0 +1,102 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		Map<Integer, List<Node>> routes = new HashMap<>();
+		for (int i = 1; i <= n; i++) {
+			routes.put(i, new ArrayList<>());
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+
+			routes.get(a).add(new Node(b, w));
+			routes.get(b).add(new Node(a, w));
+		}
+
+		int[][] map = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			map[i][i] = -1;
+			int[] visited = new int[n + 1];
+			Arrays.fill(visited, Integer.MAX_VALUE);
+			visited[i] = 0;
+
+			PriorityQueue<Route> pq = new PriorityQueue<>();
+			for (Node node : routes.get(i)) {
+				pq.add(new Route(node.to, node.to, node.weight));
+			}
+
+			while (!pq.isEmpty()) {
+				Route route = pq.poll();
+
+				if (visited[route.now] > route.weight) {
+					visited[route.now] = route.weight;
+					map[i][route.now] = route.first;
+
+					for (Node node : routes.get(route.now)) {
+						pq.add(new Route(route.first, node.to, route.weight + node.weight));
+					}
+				}
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i == j) {
+					sb.append("-");
+				} else {
+					sb.append(map[i][j]);
+				}
+				sb.append(" ");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	public static class Route implements Comparable<Route> {
+		int first;
+		int now;
+		int weight;
+
+		public Route(int first, int now, int weight) {
+			this.first = first;
+			this.now = now;
+			this.weight = weight;
+		}
+
+		public int compareTo(Route r) {
+			return Integer.compare(this.weight, r.weight);
+		}
+	}
+
+	public static class Node {
+		int to;
+		int weight;
+
+		public Node(int to, int weight) {
+			this.to = to;
+			this.weight = weight;
+		}
+	}
+}
