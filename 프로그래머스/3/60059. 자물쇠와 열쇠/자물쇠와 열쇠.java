5b45440fe359f684dc25e879d4a1eecd4b13c854
Hyunduk <kkang13277@naver.com>
2024-02-08 00:15:46 +0900

    Update 자물쇠와 열쇠.java
    [level 3] Title: 자물쇠와 열쇠, Time: 0.07 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -5,16 +5,19 @@ class Solution {
 		boolean answer = false;
 		for (int i = 0; i < 4; i++) {
 			int[][] rotate = new int[N][N];
+			// 기본 상태
 			if (i != 0) {
 				for (int r = 0; r < N; r++) {
 					for (int c = 0; c < N; c++) {
+						// 시계방향 90도 회전
 						rotate[c][N - 1 - r] = key[r][c];
 					}
 				}
 				key = rotate;
 			}
-			
+
 			if (check(key, lock)) {
+				// open 가능하면 즉시 종료
 				answer = true;
 				break;
 			}
@@ -29,6 +32,7 @@ class Solution {
 
 		int[][] board = new int[M][M];
 
+		// key가 lock 밖에 위치 하는 경우 -> N + M
 		for (int ksr = 0; ksr < N + M; ksr++) {
 			for (int ksc = 0; ksc < N + M; ksc++) {
 				for (int i = 0; i < M; i++) {
@@ -49,6 +53,7 @@ class Solution {
 				}
 
 				if (checkOpen(board)) {
+					// open 가능하면 즉시 종료
 					return true;
 				}
 			}
@@ -57,6 +62,7 @@ class Solution {
 		return false;
 	}
 
+	// lock이 모두 1이 되는지 확인
 	public boolean checkOpen(int[][] board) {
 		int N = board.length;
 		for (int i = 0; i < N; i++) {
