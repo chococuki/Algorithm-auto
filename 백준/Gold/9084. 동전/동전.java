9b89a0620b4dd162d936af9684db704f0d8492db
Hyunduk <kkang13277@naver.com>
2024-10-18 21:00:32 +0900

    [Gold V] Title: 동전, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -4,39 +4,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static StringTokenizer st;
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		// testcase
+		st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
 
-		int TC = Integer.parseInt(br.readLine());
-
-		for (int t = 0; t < TC; t++) {
-			int count = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			// coin 갯수
 			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
 
-			int[] coin = new int[count]; // 가진 동전 리스트
-
-			for (int n = 0; n < count; n++) {
-				coin[n] = Integer.parseInt(st.nextToken());
+			int[] coins = new int[N];	// 코인 종류 배열
+			st = new StringTokenizer(br.readLine());
+			for (int n = 0; n < N; n++) {
+				coins[n] = Integer.parseInt(st.nextToken());
 			}
 
-			int capa = Integer.parseInt(br.readLine()); // 만들어야 하는 금액
+			st = new StringTokenizer(br.readLine());
+			int M = Integer.parseInt(st.nextToken());
 
-			int[] dp = new int[capa + 1];
-			dp[0] = 1;
+			int[] price = new int[M + 1];	// 0~M원 까지의 경우의 수
+			price[0] = 1;
 			
-			for (int i = 0; i < count; i++) {
-				for (int j = coin[i]; j < capa + 1; j++) {
-					if (j - coin[i] >= 0) {
-						dp[j] += dp[j-coin[i]];
-					}
+			// coin을 사용해서 만들 수 있는 경우의 수
+			for (int coin : coins) {
+				for (int i = coin; i <= M; i++) {
+					price[i] += price[i - coin];
 				}
 			}
 
-			System.out.println(dp[capa]);
+			System.out.println(price[M]);
 		}
-
 	}
-
 }
